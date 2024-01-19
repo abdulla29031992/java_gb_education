9d6d172808e0002970e097dca6c1d4daa2edb8a8
абдулла <mr.ad1992@mail.ru>
2024-01-19 16:46:17 +0300

    modified programm.java

--- programm.java
+++ programm.java
@@ -28,7 +28,7 @@
         
 //     }
  
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
     
 // // типы данных 
 
@@ -54,7 +54,7 @@
 //     }    
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // вещественный числа
 // // при типе данных типа float необходимо добавить суффикс f
@@ -69,7 +69,7 @@
 //     }    
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // // тип данных типа char
@@ -85,7 +85,7 @@
 //     }    
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // тип данных типа boolean
 // public class programm {
@@ -103,6 +103,7 @@
 //     }    
 // }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // неявная типизация
 // public class programm {
@@ -116,7 +117,6 @@
 
 //     }    
 // }
-
 
 
 // public class programm {
@@ -138,7 +138,7 @@
 //     }
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // классы-обертки
 // public class programm {
@@ -150,7 +150,7 @@
 //     }    
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // операции java
 // public class programm {
@@ -163,7 +163,6 @@
 // }
 
 
-
 // // операции java: сравнение
 // public class programm {
 //     public static void main(String[] args)
@@ -174,7 +173,6 @@
 // }
 
 
-
 // // операции java: побитовые сдвиг
 // public class programm {
 //     public static void main(String[] args)
@@ -183,7 +181,6 @@
 //       System.out.println(a << 1);
 //     }    
 // }
-
 
 
 // // операции java: побитовые операции
@@ -199,7 +196,6 @@
 //     }    
 // }
 
-
 // // операции java: побитовые операции
 // public class programm {
 //     public static void main(String[] args)
@@ -214,8 +210,7 @@
 // }
 
 
-
-// // операции java: побитовые операции
+// // операции java: побитовые операции сдвиги
 // public class programm {
 //     public static void main(String[] args)
 //     {
@@ -228,7 +223,7 @@
 //     }    
 // }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // одномерные массивы
 // public class programm {
@@ -243,7 +238,6 @@
 // }
 
 
-
 // // одномерные массивы : выделение индекса 
 // public class programm {
 //     public static void main(String[] args)
@@ -253,7 +247,6 @@
 
 //     }    
 // }
-
 
 
 // // одномерные массивы : присвоение
@@ -267,6 +260,7 @@
 //     }    
 // }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // // многомерные массивы
 // public class programm {
@@ -299,7 +293,36 @@
 // }
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 // преобразование : можно ?
+
+// public class programm {
+//     public static void main(String[] args) {
+//         int i = 123; double d = i;
+//         System.out.println(i);  // 123
+//         System.out.println(d);  // 123.0
+        
+//         d = 3.1415; i = (int)d;
+//         System.out.println(d);  // 3.1415
+//         System.out.println(i);  // 3
+        
+//         d = 3.9415; i = (int)d;
+//         System.out.println(d);  // 3.9415
+//         System.out.println(i);  // 3
+        
+//         byte b = Byte.parseByte("123");
+//         System.out.println(b);  // 123
+        
+//         b = Byte.parseByte("1234");
+//         System.out.println(b);  // NumberFormatException: Value out of range
+//     }
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // получение данных их терминала
 // import java.util.Scanner;
@@ -312,6 +335,36 @@
 // iScanner. close () ;
 // }
 // }
+
+// получение данных их терминала : некоторые примитивы
+
+// import java.util.Scanner;
+// public class programm {
+//     public static void main(String[] args) {
+//         Scanner iScanner = new Scanner(System.in);
+//         System.out.printf("int a: ");
+//         int x = iScanner.nextInt();
+//         System.out.printf("double a: ");
+//         double y = iScanner.nextDouble();
+//         System.out.printf("%d + %f = %f", x, y, x + y);
+//         iScanner.close();
+// }}
+
+// получение данных их терминала : проверка на соответствие получаемого типа
+// import java.util.Scanner;
+// public class programm {
+// public static void main(String[] args) {
+// Scanner iScanner = new Scanner(System.in);
+// System.out.printf("int a: ");
+// boolean flag = iScanner.hasNextInt();
+// System.out.println(flag);
+// int i = iScanner.nextInt();
+// System.out.println(i);
+// iScanner.close();
+// }}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // форматированный вывод 
@@ -335,4 +388,304 @@
 //         System.out.printf("%d + %d = %d \n", a, b, c);
 //         System.out.println(res);
 //      }
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// виды спецификаторов
+
+// public class programm {
+//     public static void main(String[] args) {
+//         float pi = 3.1415f;
+// System.out.printf("%f\n", pi);    // 3,141500
+// System.out.printf("%.2f\n", pi);  // 3,14
+// System.out.printf("%.3f\n", pi);  // 3,141
+// System.out.printf("%e\n", pi);    // 3,141500e+00
+// System.out.printf("%.2e\n", pi);  // 3,14e+00
+// System.out.printf("%.3e\n", pi);   // // 3,141e+00
+
+// } }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// область видимых переменных
+
+
+// public class programm {
+//     public static void main(String[] args) {
+//  {
+//  int i = 123;
+//             System.out.println(i);
+//         }
+//         System.out.println(i);  // error: cannot find symbol
+//     }
+//  }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Функции и методы
+
+// public class programm {
+//     static void sayHi() {
+//         System.out.println("hi!");
+//     }
+//     static int sum(int a, int b) {
+//         return a+b;
+//     }
+//     static double factor(int n) {
+//         if(n==1)return 1;
+//         return n * factor(n-1);
+//     }
+//     public static void main(String[] args) {
+//         sayHi(); // hi!
+//         System.out.println(sum(1, 3)); // 4
+//         System.out.println(factor(5)); // 120.0
+//     }}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Управляющий конструкции
+
+// условный оператор
+// public class programm {
+//     public static void main(String[] args) 
+//     {
+//         int a = 1;
+//         int b = 2;
+//         int c;
+//         if (a > b) 
+//         {
+//         c = a; 
+//         } 
+//         else 
+//         {
+// c = b; }
+//         System.out.println(c);
+//         }
+//     }
+
+
+// условный оператор
+// public class programm {
+//         public static void main(String[] args) 
+//         {
+//             int a = 1;
+//             int b = 2;
+//             int c = 0;
+//             if (a > b) c = a;
+//             if (b > a) c = b;
+//             System.out.println(c);
+//         }
+//     }
+
+
+// // тернарный оператор
+// public class programm 
+// {
+//     public static void main(String[] args) 
+//     {
+//     int a = 1;
+//     int b = 2;
+//     int min = a < b ? a : b;
+//     System.out.println(min);
+//     } 
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// оператор выбора
+
+// import java.util.Scanner;
+// public class programm  
+// {
+//     public static void main(String[] args)
+//     {
+//         int mounth = value;
+//         String text = "";
+//         switch (mounth) 
+//         {
+//             case 1:
+//             text = "Autumn";
+//             break;
+        
+//             default:
+//             text = "mistake";
+//             break;
+//         }
+//             System.out.println(text);
+//             iScanner.close();
+//         } 
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Циклы
+
+// цикл while
+// public class programm 
+// {
+//     public static void main(String[] args) 
+//     {
+//         int value = 321;
+//         int count = 0;
+//         while (value != 0) 
+//     {
+//             value /= 10;
+//             count++; 
+//     }
+//         System.out.println(count);
+//     }
+// }
+
+
+// цикл do while
+// public class programm 
+// {
+//     public static void main(String[] args) 
+//     {
+
+//         int value = 321;
+//         int count = 0;
+//         do 
+//     {
+//         value /= 10;
+//         count++;
+//     }              
+//         while (value != 0);
+//         System.out.println(count);
+//     } 
+// }
+
+
+// цикл for
+// public class programm
+// {
+//     public static void main(String[] args) 
+//     {
+//         int s = 0;
+//         for (int i = 1; i <= 10; i++) 
+//     {
+//         s += i; 
+//     }
+//         System.out.println(s);
+//     }
+// }
+
+
+// вложенные циклы
+// public class programm
+// {
+//     public static void main(String[] args) 
+//     {
+
+//         for (int i = 0; i < 5; i++) 
+//         {
+//             for (int j = 0; j < 5; j++) 
+//             {
+//                 System.out.print("* ");
+//             }
+//         }
+//             System.out.println();
+//     }   
+// }
+
+
+// for работает только для коллекций
+// public class programm 
+// {
+//     public static void main(String[] args) 
+//     {
+//         int arr[] = new int[10];
+//         for (int item : arr) 
+//         {
+//             System.out.printf("%d ", item);
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Работа с файлами
+
+// Создание и запись\ дозапись
+// import java.io.FileWriter;
+// import java.io.IOException;
+
+//  public class programm 
+//  {
+//      public static void main(String[] args) 
+//      {
+//          try (FileWriter fw = new FileWriter("file.txt", false)) 
+//          {
+//              fw.write("line 1");
+//              fw.append('\n');
+//              fw.append('2');
+//              fw.append('\n');
+//              fw.write("line 3");
+//              fw.flush();
+//          } 
+//             catch (IOException ex) 
+//             {
+//                 System.out.println(ex.getMessage());
+//             }
+//         }
+//  }      
+
+
+// Чтение, вариант посимвольно
+// import java.io.*;
+
+//   public class programm 
+//   {
+//       public static void main(String[] args) throws Exception 
+//       {
+//         FileReader fr = new FileReader("file.txt");
+//           int c;
+          
+//         while ((c = fr.read()) != -1)
+//             {
+//                 char ch = (char) c;
+//                 if (ch == '\n') 
+//             {
+//                 System.out.print(ch);
+//             } 
+//             else 
+//             {
+//                 System.out.print(ch);
+//             }
+//             }
+//       }
+//   }
+
+
+// Вариант посрочно
+// import java.io.*;
+
+//   public class programm 
+//   {
+//       public static void main(String[] args) throws Exception 
+//       {
+//           BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+//           String str;
+//           while ((str = br.readLine()) != null) 
+//             {
+//                 System.out.printf("== %s ==\n", str);
+//             }
+//           br.close();
+//       }
 // }
