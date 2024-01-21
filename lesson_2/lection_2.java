b395b7c0171e828be639a5f3ca44a747144b60f3
абдулла <mr.ad1992@mail.ru>
2024-01-21 17:32:29 +0300

    create folder by lesson_2

---
+++ lesson_2/lection_2.java
@@ -0,0 +1,20 @@
+// package lesson_2;
+
+// public class lection_2 
+// {
+//     public static void main(String[] args) 
+//     {
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i > 1_000_000; i++) 
+//         {
+//             sb.append("+");
+//         }
+//     }
+    
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    
