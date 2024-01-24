3539d35f09dd19288a79bbcd80087185aa060302
абдулла <mr.ad1992@mail.ru>
2024-01-25 00:24:42 +0300

    modified seminar_2

--- lesson_2/seminar_2.java
+++ lesson_2/seminar_2.java
@@ -112,7 +112,84 @@
 // Напишите метод который составит строку, состоящую из 100 повторений слова TEST
 // и метод, который запишет эту строку в простой текстовый файл, обработайте исключения.
 
+// import java.io.File;
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.util.Scanner;
+// import java.util.logging.FileHandler;
+// import java.util.logging.Handler;
+// import java.util.logging.Logger;
+// import java.util.logging.SimpleFormatter;
 
 
+// public class seminar_2 {
+//     static Logger logger = Logger.getAnonymousLogger();
+
+//     public static void main(String[] args) {
+//         String s = "test";
+//         String filePath = "lesson_2/test.txt";
+//         String logPath = "lesson_2/log.txt";
+//         int n = 10;
+
+//         createLogger(logPath);
+
+//         String s1 = repeat(s, n);
+//         writeToFile(s1, filePath);
+//         System.out.println("конец программы");
+
+//         closeLogger();
+//     }
+
+//     static void createLogger(String logPath){
+//         try {
+//             FileHandler fh = new FileHandler(logPath, true);
+//             SimpleFormatter simpleFormatter = new SimpleFormatter();
+//             fh.setFormatter(simpleFormatter);
+//             logger.addHandler(fh);
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+
+//     static void closeLogger(){
+//         Handler[] handlers = logger.getHandlers();
+//         for (Handler handler: handlers){
+//             handler.close();
+//         }
+//     }
+
+//     static void writeToFile(String s, String filePath) {
+//         try (FileWriter fileWriter = new FileWriter(filePath, true)){
+//             fileWriter.write(s);
+//             fileWriter.write("\n");
+//             logger.info("запись файла прошла успешна");
+//         } catch (IOException e) {
+//             logger.warning("не удалось призвести запись файла");
+//             e.printStackTrace();
+//         }
+//     }
+
+//     static String readFile(String filePath){
+//         StringBuilder stringBuilder = new StringBuilder();
+//         File file = new File(filePath);
+//         try (Scanner scanner = new Scanner(file)){
+//             while (scanner.hasNext()){
+//                 stringBuilder.append(scanner.nextLine());
+//             }
+//         } catch (IOException e){
+//             e.printStackTrace();
+//         }
+//         return stringBuilder.toString();
+//     }
+
+//     static String repeat(String s, int n){
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < n; i++) {
+//             sb.append(s);
+//             sb.append(" ");
+//         }
+//         return sb.toString();
+//     }
+// }
 
 
