1717d6334a0b578aaac6f9e1e128f02ef9bb2163
абдулла <mr.ad1992@mail.ru>
2024-02-02 22:42:27 +0300

    create seminar_5

---
+++ lesson_5/pasports.java
@@ -0,0 +1,45 @@
+package lesson_5;
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class pasports 
+// {
+//     Map<String, String> map = new HashMap<>();
+    
+//     void add(String pass_number, String last_name)
+//     {
+//         map.put(pass_number, last_name);
+//     }
+
+   
+//     public String toString() 
+//     {
+//         StringBuilder stringBuilder = new StringBuilder();
+//         for (String element : map.keySet())
+//         {
+//             stringBuilder.append(element);
+//             stringBuilder.append(" : ");
+//             stringBuilder.append(map.get(element));
+//             stringBuilder.append("\n");
+//         }
+//         return stringBuilder.toString();
+//     }
+
+//     String Find_by_Name(String last_name)
+//     {
+//         StringBuilder stringBuilder = new StringBuilder();
+//         for (String element : map.keySet())
+//         {
+//             if (map.get(element) .equals(last_name))
+//             {
+//                 stringBuilder.append(element);
+//                 stringBuilder.append(" : ");
+//                 stringBuilder.append(map.get(element));
+//                 stringBuilder.append("\n");
+//             }
+//         }
+//         return stringBuilder.toString();
+//     }
+// }
+
---
+++ lesson_5/seminar_5.java
@@ -0,0 +1,30 @@
+package lesson_5;
+
+
+// Создать структуру для хранения Номеров паспортов и Фамилий сотрудников организации.
+// 123456 Иванов 
+// 321456 Васильев 
+// 234561 Петрова 
+// 234432 Иванов 
+// 654321 Петрова 
+// 345678 Иванов
+// Вывести данные по сотрудникам с фамилией Иванов.
+
+
+// public class seminar_5 
+// {
+//     public static void main(String[] args) 
+//     {
+//         pasports pasports = new pasports();
+//         pasports.add("123456", "Иванов");
+//         pasports.add("321456", "Васильев ");
+//         pasports.add("234561", "Петрова ");
+//         pasports.add("234432", "Иванов ");
+//         pasports.add("654321", "Петрова ");
+//         pasports.add("345678", "Иванов");
+
+//         System.out.println(pasports);
+//         System.out.println(pasports.Find_by_Name("Иванов"));
+//     }
+    
+// }
