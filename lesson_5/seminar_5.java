3561ade5389c97e3002db577c3996fefee5d82a8
абдулла <mr.ad1992@mail.ru>
2024-02-03 14:52:47 +0300

    modified seminar_5

--- lesson_5/seminar_5.java
+++ lesson_5/seminar_5.java
@@ -1,6 +1,5 @@
 package lesson_5;
 
-
 // Создать структуру для хранения Номеров паспортов и Фамилий сотрудников организации.
 // 123456 Иванов 
 // 321456 Васильев 
@@ -26,5 +25,104 @@ package lesson_5;
 //         System.out.println(pasports);
 //         System.out.println(pasports.Find_by_Name("Иванов"));
 //     }
-    
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Даны 2 строки, написать метод, который вернет true, если эти строки являются изоморфными и false, если нет. 
+// Строки изоморфны, если одну букву в первом слове можно заменить на некоторую букву во втором слове, при этом:
+// 1. повторяющиеся буквы одного слова меняются на одну и ту же букву с сохранением порядка следования. (Например, add - egg изоморфны)
+// 2. буква может не меняться, а остаться такой же. (Например, note - code)
+// Пример 1:
+// Input: s = "foo", t = "bar" Output: false
+// Пример 2:
+// Input: s = "paper", t = "title" Output: true
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class seminar_5 
+// {
+//     public static void main(String[] args) 
+//     {
+//         System.out.println(isIsomorphic("add", "egg"));
+//         System.out.println(isIsomorphic("note", "code"));
+//         System.out.println(isIsomorphic("paper", "title"));
+//         System.out.println(isIsomorphic("foo", "bar"));
+//         System.out.println(isIsomorphic("bar", "foo"));
+//     }
+
+//     static boolean isIsomorphic(String s_1, String s_2)
+//     {
+//         if (s_1.length() != s_2.length())
+//         {
+//             return false;
+//         }
+
+//         Map<Character, Character> map = new HashMap<>();
+//         for (int i = 0; i < s_1.length(); i++) 
+//         {
+//             if (map.containsKey(s_1.charAt(i)))
+//             {
+//                 if (s_2.charAt(i) != map.get(s_1.charAt(i)))
+//                 {
+//                     return false;
+//                 }
+//             }
+//             else
+//             {
+//                 if (map.containsValue(s_2.charAt(i)))
+//                 {
+//                     return false;
+//                 }
+//                 map.put(s_1.charAt(i), s_2.charAt(i));   // s_1-ключ  s_2-значение
+//             }
+//         }
+//         return true;
+//     }   
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Написать программу, определяющую правильность расстановки скобок в выражении. Пример 1: a+(d*3) - истина
+Пример 2: [a+(1*3) - ложь
+Пример 3: [6+(3*3)] - истина
+Пример 4: {a}[+]{(d*3)} - истина Пример 5: <{a}+{(d*3)}> - истина Пример 6: {a+]}{(d*3)} - ложь
+*/
+
+// import java.util.Stack;
+
+// public class seminar_5 
+// {
+//     public static void main(String[] args) 
+//     {
+//         System.out.println(checkBrackets("[a+(1*3)"));
+//         System.out.println(checkBrackets("[6+(3*3)]"));
+//         System.out.println(checkBrackets("{a}[+]{(d*3)}"));
+        
+//     }
+
+//     static boolean checkBrackets(String str)
+//     {
+//         Stack<Character> stack = new Stack<>();
+//         for (Character s : str.toCharArray()) 
+//         {
+//             if (s == '(') 
+//             {
+//                 stack.add(s);
+//             }
+//             else if (s == ')')
+//             {
+//                 if (stack.pop() != '(')
+//                 {
+//                     return false;
+//                 }
+                
+//             }
+//         }
+//         return true;
+//     } 
 // }
