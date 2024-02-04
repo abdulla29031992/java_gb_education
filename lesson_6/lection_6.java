35aa690b4a152138a2ac9b1652b7a0e86c15ae07
абдулла <mr.ad1992@mail.ru>
2024-02-04 19:02:33 +0300

    create folder of lesson_6

---
+++ lesson_6/lection_6.java
@@ -0,0 +1,42 @@
+package lesson_6;
+
+// import java.util.HashSet;
+// import java.util.Set;
+
+// Set
+// public class lection_6 
+// {
+//     public static void main(String[] args) 
+//     {
+//         Set< Integer> set = new HashSet<> ();
+//         set.add (1); set.add (12); set.add (123) ; set.add (1234) ; set.add (1234) ;
+//         System.out.println (set.contains (12)); // true set. add (null);
+//         System.out.println(set.size ()); // 5
+//         System.out.println(set); // [null, 1, 1234, 123, 121 set. remove (12) ;
+//         for (var item: set) System.out.println(item); // null 1 1234 123 set.clear ();
+//         System.out.println (set);    
+//     }    
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// import java.util.Arrays;
+// import java.util.HashSet;
+
+// HashSet
+// public class lection_6 
+// {
+//     public static void main(String[] args) 
+//     {
+//         var a = new HashSet<> (Arrays.asList (1,2,3,4,5, 6,7)) ;
+//         var b = new HashSet<> (Arrays.asList (2,3, 5,7, 11,13,17)) ;
+//         var u = new HashSet<Integer> (a); u.addAll (b);
+//         var r = new HashSet<Integer> (a); r.retainAll (b);
+//         var s = new HashSet<Integer>(a);  s.removeAll(b);
+//         System.out.println(a); 
+//         System.out.println(b); 
+//         System.out.println (u);
+//         System.out.println (r);
+//         System.out.println (s) ;        
+//     }    
+// }
