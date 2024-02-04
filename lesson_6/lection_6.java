a9cb6fcec987959657dda3d5121829b4c5c30f03
абдулла <mr.ad1992@mail.ru>
2024-02-04 19:41:09 +0300

    create file worker

--- lesson_6/lection_6.java
+++ lesson_6/lection_6.java
@@ -40,3 +40,52 @@ package lesson_6;
 //         System.out.println (s) ;        
 //     }    
 // }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// import java.util.Arrays;
+// import java.util.HashSet;
+
+// public class lection_6 
+// {
+//     public static void main(String[] args) 
+//     {
+//         worker w1 = new worker();
+//         w1.first_name = "Имя_1";
+//         w1.last_name = "Фамилия_1";
+//         w1.salary = 100;
+//         w1.id = 1000; 
+
+//         worker w4 = new worker();
+//         w4.first_name = "Имя_1";
+//         w4.last_name = "Фамилия_1";
+//         w4.salary = 100;
+//         w4.id = 1000;
+
+//         worker w2 = new worker();
+//         w2.first_name = "Имя_2";
+//         w2.last_name = "Фамилия_2";
+//         w2.salary = 200;
+//         w2.id = 2000;
+
+//         worker w3 = new worker();
+//         w3.first_name = "Имя_3";
+//         w3.last_name = "Фамилия_3";
+//         w3.salary = 300;
+//         w3.id = 3000;
+
+
+//         System.out.println(w1.toString());
+//         System.out.println(w2);
+//         System.out.println(w3);
+//         System.out.println(w4);
+
+//         System.out.println(w1 = w4);
+//         System.out.println(w1.equals(w4));
+//         var workers = new HashSet<worker>(Arrays.asList(w1, w2, w3));
+//         System.out.println(workers.contains(w4));
+
+//     }
+    
+// }
---
+++ lesson_6/worker.java
@@ -0,0 +1,31 @@
+// package lesson_6;
+
+// public class worker 
+// {
+//     int id;
+//     int salary;
+//     String first_name;
+//     String last_name;
+    
+    
+//     @Override
+//     public String toString() 
+//     {
+//         return String.format(" id:%d fn:%s sn:%s s:%d", id, first_name, last_name, salary);
+         
+//     }
+
+//     // @Override
+//     // public boolean equals(Object o) 
+//     // {
+//     //     var t =( worker) o;
+//     //     return id = t.id && first_name = t.first_name;
+//     // }
+
+//     @Override
+//     public int hashCode() 
+//     {
+        
+//         return super.hashCode();
+//     }
+// }
